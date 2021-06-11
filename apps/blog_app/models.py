fbc98a61a791810864f2fe194b9fcd1b0e21f75c
Vicente Chacón <vicentechacon@MacBook-Air-de-Vicente.local>
2021-06-10 20:07:55 -0400

    Dejando base de datos operativa

--- apps/blog_app/models.py
+++ apps/blog_app/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
+
 class AbstractModel(models.Model):
     def __str__(self):
         return str(self.__dict__)
     class Meta:
         abstract = True
 
-class Usuario(AbstractModel):
-    nombre = models.CharField(max_length=255)
-    apellido = models.CharField(max_length=255)
-    fecha_nacimiento = models.DateField()
-    descripcion = models.TextField()
-    post_likeados = models.ManyToManyField('Post', through='Like', related_name='usuarios')
 
 class PostManager(models.Manager):
     def validacion_basica(self, post_data):
@@ -42,8 +37,8 @@ class Comentario(AbstractModel):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Like(AbstractModel):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='likes')
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    usuario = models.ForeignKey('login_project_app.Usuario', on_delete=models.CASCADE, related_name='likes')
 
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog_app.urls')),
+    path('login/', include('apps.login_project_app.urls')),
 ]
